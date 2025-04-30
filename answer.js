e9d9eefc61d3c1cd61f1ef153b77f28090fff0cf
RyanGomesMelo <00001123927078SP@al.educacao.sp.gov.br>
2025-04-30 18:49:51 -0300

    Update answer.js

--- answer.js
+++ answer.js
@@ -156,7 +156,7 @@ await loadCss('https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css');
 
 // Carrega o Toastify e inicia as funcionalidades
 loadScript('https://cdn.jsdelivr.net/npm/toastify-js').then(async () => {
-    sendToast("Injetado com Sucesso!", 5000, 'bottom');
+    sendToast("Injetado com Sucesso! Code: Ryan", 5000, 'bottom');
     const originalFetch = window.fetch;
     
     const targetRegex = /^https:\/\/edusp-api\.ip\.tv\/tms\/task\/\d+\/answer$/;
@@ -182,7 +182,7 @@ loadScript('https://cdn.jsdelivr.net/npm/toastify-js').then(async () => {
     async function ReplayAnswer(url, data) {
       await responderCorretamente(await pegarRespostas(data.id, data.task_id), data.id, data.task_id)
       const oldTitle = document.title
-      document.title = "Fandangos"
+      document.title = "Ryan <3"
       setTimeout(() => {
       	document.title = oldTitle
       }, 2000)
